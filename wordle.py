433370510acb7e8fbd066a945a1a111089bd4ba5
Sebastián Peralta <sebastianperaltaaguirre@gmail.com>
2026-01-28 15:24:06 -0300

    Acoplo el código que trabajamos con Maggi para iniciar a codear

---
+++ wordle.py
@@ -0,0 +1,24 @@
+letras_verificadas = []
+cantidad_letras = 5
+intentos = 0
+
+def verificador_palabra(palabra_ingresada, palabra_secreta):
+    for i in range(cantidad_letras):
+        las_palabras_son_iguales = palabra_ingresada[i] == palabra_secreta[i] # True o False
+        la_letra_existe_en_la_palabra = palabra_ingresada[i] in palabra_secreta 
+        
+        if las_palabras_son_iguales:
+            letras_verificadas.append(f"[{palabra_ingresada[i]}]")
+        elif la_letra_existe_en_la_palabra:
+            letras_verificadas.append(f"({palabra_ingresada[i]})")
+        else:
+            letras_verificadas.append(palabra_ingresada[i])
+    
+    print(letras_verificadas)
+
+while intentos < 6:
+    print(f"\nte quedan {6 - intentos} intentos")
+    intentos = intentos + 1
+    palabra_ingresada = input("Ingrese una palabra: ")
+    print(f"la palabra ingresada es: {palabra_ingresada}")
+    verificador_palabra(palabra_ingresada, 'calor')
